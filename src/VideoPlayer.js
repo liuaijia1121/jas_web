1dc29f8d17211f80b39eeb38b1ce09a01002a25b
liuaijia <ge95hul@tum.de>
2023-09-18 22:43:06 +0800

    确保路径正确

--- src/VideoPlayer.js
+++ src/VideoPlayer.js
@@ -4,7 +4,7 @@ function VideoPlayer() {
     return (
         <div>
             <video width="100%" height="480" controls>
-                <source src="/img/反方向的钟.mp4" type="video/mp4" />
+                <source src={process.env.PUBLIC_URL + "/img/反方向的钟.mp4"} type="video/mp4" />
                 您的浏览器不支持视频标签。
             </video>
         </div>
